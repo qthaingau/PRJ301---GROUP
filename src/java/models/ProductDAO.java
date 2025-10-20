8269cc167a82e0b0fd65d6a1fbac566ae392b60c
Tran Quoc Thai <tqt12342018s@gmail.com>
2025-10-20 09:37:02 +0700

    QT lam ne

--- src/java/models/ProductDAO.java
+++ src/java/models/ProductDAO.java
@@ -43,4 +43,97 @@ public class ProductDAO {
         }
         return listProduct;
     }
+
+    public ProductDTO getProductByID(String productID) {
+        try {
+            Connection conn = DBUtils.getConnection();
+            String sql = "SELECT * FROM Product WHERE productID = ?";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, productID);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                ProductDTO product = new ProductDTO();
+                product.setProductID(rs.getString("productID"));
+                product.setProductName(rs.getString("productName"));
+                product.setDescription(rs.getString("description"));
+                product.setCategoryID("categoryID");
+                product.setBrandID(rs.getString("brandID"));
+                product.setCreatedAt(rs.getDate("createdAT").toLocalDate());
+                product.setIsActive(rs.getBoolean("isActive"));
+                return product;
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public List<ProductDTO> getProductByName(String productName) {
+        List<ProductDTO> listProduct = new ArrayList<>();
+        try {
+            Connection conn = DBUtils.getConnection();
+            String sql = "SELECT * FROM Product WHERE productName LIKE ?";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, productName);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                ProductDTO product = new ProductDTO();
+                product.setProductID(rs.getString("productID"));
+                product.setProductName(rs.getString("productName"));
+                product.setDescription(rs.getString("description"));
+                product.setCategoryID("categoryID");
+                product.setBrandID(rs.getString("brandID"));
+                product.setCreatedAt(rs.getDate("createdAT").toLocalDate());
+                product.setIsActive(rs.getBoolean("isActive"));
+                listProduct.add(product);
+            }
+        } catch (Exception e) {
+        }
+        return listProduct;
+    }
+
+    public boolean sortDelete(String productID) {
+        try {
+            Connection c = DBUtils.getConnection();
+            String sql = "UPDATE FROM Product SET isActive = 0"
+                    + "WHERE userID = ?";
+
+            PreparedStatement pst = c.prepareStatement(sql);
+            pst.setString(1, productID);
+
+            int i = pst.executeUpdate();
+            return i > 0;
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    public boolean update(ProductDTO productDTO) {
+        try {
+            Connection c = DBUtils.getConnection();
+            String sql = "UPDATE Product "
+                    + "SET productName = ?"
+                    + ", description = ?"
+                    + ", categoryID = ?"
+                    + ", brandID = ?"
+                    + ", createdAT = ?"
+                    + ", isActive = ?"
+                    + "WHERE productID = ?";
+
+            PreparedStatement pst = c.prepareStatement(sql);
+            pst.setString(1, productDTO.getProductName());
+            pst.setString(2, productDTO.getDescription());
+            pst.setString(3, productDTO.getCategoryID());
+            pst.setString(4, productDTO.getBrandID());
+            pst.setDate(5, java.sql.Date.valueOf(productDTO.getCreatedAt()));
+            pst.setBoolean(6, productDTO.isIsActive());
+            pst.setString(7, productDTO.getProductID());
+            
+            int i = pst.executeUpdate();
+            return i > 0;
+        } catch (Exception e) {
+        }
+        return false;
+    }
 }
