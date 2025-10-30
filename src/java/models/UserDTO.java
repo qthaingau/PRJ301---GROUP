1cbbb99fe440d58aefcf3c8f0d3d3fc32ee66490
tuanhai-se <tuanhainguyen.103@gmail.com>
2025-10-30 10:05:45 +0700

    TH up UserDTO

--- src/java/models/UserDTO.java
+++ src/java/models/UserDTO.java
@@ -4,12 +4,99 @@
  */
 package models;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Admin
  */
 public class UserDTO {
-    private String abc;
-    private String xyz;
-    private String hi;
+    private String userID;
+    private String username;
+    private String email;
+    private String password;
+    private String fullName;
+    private String phoneNumber;
+    private String role;
+    private LocalDate createdAt;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String userID, String username, String email, String password, String fullName, String phoneNumber, String role, LocalDate createdAt) {
+        this.userID = userID;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+        this.createdAt = createdAt;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    
 }
